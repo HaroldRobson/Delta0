70a74f22a0ce80bb67478de28ae181a0a7f49551
FergusJJ <fergus@joinblink.com>
2026-01-16 18:17:11 +0000

    docs: comments

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import Button from "./Button/Button";
+import Button from "./components/Button/Button";
 import Banner from "./components/Banner/Banner";
 import s from "./App.module.css";
 /*
@@ -11,8 +11,29 @@ import s from "./App.module.css";
  * - display current yield
  *   - want some copy in middle with what the project is about
  * - probably want some reusable btn components
- *
- *
+  a
+
+  w.r.t. calling smart contracts
+  on deploy we should get the deployed contract address and
+  the functions as const
+
+  can just do useAccount from wagmi? -> makes call to metamask extension
+  user can connect their wallet via this
+
+
+  Routing -> React router
+  Can just have something like
+
+  <Route path={'/'}>
+    <HomePage />
+  </Route>
+
+
+
+  }
+
+
+
  * */
 
 function App() {
