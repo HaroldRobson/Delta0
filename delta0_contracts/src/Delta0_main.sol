f4b219303a80c231e6603af42d7969cb9927ae54
Harold Robson <wt23188@bristol.ac.uk>
2026-01-17 03:33:03 +0000

    doing things with the corewrite

--- delta0_contracts/src/Delta0_main.sol
+++ delta0_contracts/src/Delta0_main.sol
@@ -2,12 +2,15 @@ pragma solidity ^0.8.30;
 
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {PrecompileLib} from "../lib/hyper-evm-lib/src/PrecompileLib.sol";
+import {CoreWriterLib} from "../lib/hyper-evm-lib/src/CoreWriterLib.sol";
+import {HLConversions} from "../lib/hyper-evm-lib/src/common/HLConversions.sol";
 
-contract Delta0_Logic is ERC20 {
-    uint32 public asset;
+contract Delta0_main is ERC20 {
     uint32 public perpdexindex;
+    uint32 public spotdexindex;
     address public TokenAddress;
-    address public LogicAddress;
+    address public USDCAddress;
     address public RouterAddress;
     address public Owner;
     address public BBO_PRECOMPILE_ADDRESS;
@@ -16,16 +19,18 @@ contract Delta0_Logic is ERC20 {
     IERC20 public Token; // the token being hedged
 
     constructor(
-        address _LogicAddress,
+        uint32 _perpdexindex,
+        uint32 _spotdexindex, // often called "asset" in docs
         address _TokenAddress,
+        address _USDCAddress,
         address owner,
         string memory Name,
-        string memory Ticker,
-        uint32 _asset
+        string memory Ticker
     ) ERC20(Name, Ticker) {
-        asset = _asset;
+        perpdexindex = _perpdexindex;
+        spotdexindex = _spotdexindex;
         TokenAddress = _TokenAddress; // adress of token being hedged
-        LogicAddress = _LogicAddress;
+        USDCAddress = _USDCAddress;
         RouterAddress = msg.sender;
         Owner = owner;
         Token = IERC20(TokenAddress);
@@ -73,14 +78,6 @@ contract Delta0_Logic is ERC20 {
         return Token.balanceOf(address(this));
     }
 
-    function bbo() public view returns (Bbo memory) {
-        bool success;
-        bytes memory result;
-        (success, result) = BBO_PRECOMPILE_ADDRESS.staticcall(abi.encode(asset));
-        require(success, "Bbo precompile call failed");
-        return abi.decode(result, (Bbo));
-    }
-
     function accountMarginSummary() public view returns (AccountMarginSummary memory) {
         bool success;
         bytes memory result;
@@ -95,13 +92,15 @@ contract Delta0_Logic is ERC20 {
         AccountMarginSummary memory summary = accountMarginSummary();
         require(summary.rawUsd > 0, "rawUSD is negative!");
         uint64 coreValueInUSDC = uint64(summary.accountValue); // CHECK CONVERSION
-        uint256 totalUSDCValueHeldInToken = totalTokenOnEVM * bbo().bid; // check if bid or ask
+        uint64 tokenIndex = PrecompileLib.getTokenIndex(TokenAddress);
+        uint256 totalUSDCValueHeldInToken = totalTokenOnEVM * PrecompileLib.normalizedSpotPx(tokenIndex); // check if bid or ask
         return totalUSDCValueHeldInToken + coreValueInUSDC;
     }
 
     function hedge(address user, uint256 amount) public {
         Token.transferFrom(user, address(this), amount);
-        uint256 USDCValueReceived = bbo().bid * amount;
+        uint64 tokenIndex = PrecompileLib.getTokenIndex(TokenAddress);
+        uint256 USDCValueReceived = PrecompileLib.normalizedSpotPx(tokenIndex) * amount;
         if (totalSupply() != 0) {
             uint256 tokensToMint = USDCValueReceived * totalSupply() / totalUSDCValueUnderManagement();
             _mint(user, tokensToMint);
@@ -110,6 +109,20 @@ contract Delta0_Logic is ERC20 {
         }
     }
 
-    function sendToHyperCore(uint256 amount) external onlyOwner {}
-    function takeFromHyperCore(uint256 amount) external onlyOwner {}
+    function sendToHyperCore(uint256 amount) public onlyOwner {
+        CoreWriterLib.bridgeToCore(TokenAddress, amount);
+    }
+
+    function takeFromHyperCore(uint256 amount) public onlyOwner {
+        CoreWriterLib.bridgeToEvm(TokenAddress, amount);
+    }
+
+    function increaseCollateral() public onlyOwner {
+        // sell all of the Token on HyperEVM and use all as collateral
+        uint64 balance = PrecompileLib.spotBalance(address(this), TokenAddress).total;
+        CoreWriterLib.placeLimitOrder(spotdexindex, false, 0, balance, false, 3, uint128(block.timestamp)); // limit Ask at price of 0 is basically a market sell.
+        uint64 amount = PrecompileLib.spotBalance(address(this), USDCAddress).total;
+        uint64 usdcPerpAmount = HLConversions.weiToPerp(amount);
+        CoreWriterLib.transferUsdClass(usdcPerpAmount, true);
+    }
 }
